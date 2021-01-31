9cb5d8fecf02b8e1adf2ef3e4844209ecf3d96e9
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2021-01-31 13:54:37 +0530

    Added v1.0.0 release file!

--- student_report_card_management.cpp
+++ student_report_card_management.cpp
@@ -1,173 +1,384 @@
-#include <iostream>
-#include <string>
-#include <vector>
+//MINI PROJECT: STUDENT REPORT MANAGEMENT by TEAM 1
+
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include <stdlib.h>
 using namespace std;
 
-class Student
+//Student class: It will contain the student variables I have declare student's roll number, name, percentage, grade and the 5 Subjects we have in this sem.
+
+class student
 {
-private:
-	string name;
-	int roll;
-	float OOPs, ADE, DS, COSM, COA, average;
+	int rollno;
+	char name[50];
+	int DS_marks, OOPs_marks, COSM_marks, ADE_marks, COA_marks;
+	double per;
+	char grade;
+	void calculate();
 public:
+	void getdata();
+	void showdata() const;
+	void show_tabular() const;
+	int retrollno() const;
+}; //class ends here
 
-	void InsertData(string n, int r, float oop, float ade, float ds, float cosm, float coa)
-	{
-		name = n;
-		roll = r;
-		OOPs = oop;
-		ADE = ade;
-		DS = ds;
-		COSM = cosm;
-		COA = coa;
-
-		average = (OOPs + ADE + DS + COSM + COA) / 5;
-	}
-	
-
-
-//<---------Grade calculation section completed------->
-	int GetRoll()
-	{
-		return roll;
-	}
-
-	void Report()
-	{
-		cout << "\nBIET STUDENT REPORT" << endl;
-		cout << "==============" << endl << endl;
-		cout << "Name: " << name << endl;
-		cout << "Roll Number: " << roll << endl;
-		cout << "OOPs Marks: " << OOPs << endl;
-		cout << "ADE Marks: " << ADE << endl;
-		cout << "DS Marks: " << DS << endl;
-		cout << "COSM Marks: " << COSM << endl;
-		cout << "COA Marks: " << COA << endl;
-		cout << "Average Marks: " << average << endl;
-		cout << "GPA: " << average / 10 << endl;
-	}
-};
-
-//Vector is used instead of an array due to its flexibility in size extension
-
-vector<Student> database;
-int databaseSize;
-
-void PrintReport(int roll)
+// Since our student function has variables that are only limited to that function, we will use scope resolution function to access marks of DS,OOPs,COSM,ADE,COA subjects.
+void student::calculate()
 {
-	for (int i = 0; i < databaseSize; i++)
-		if (database[i].GetRoll() == roll)
-		{
-			database[i].Report();
-			return;
-		}
-
-	cout << "\nTHE ENTERED ROLL NUMBER:(" << roll << ") DOES NOT EXIST.\n" << endl;
+	per=(DS_marks+OOPs_marks+COSM_marks+ADE_marks+COA_marks)/5.0;
+	if(per>=90)
+		grade='O';
+	else if(per>=70&&per<90)
+		grade='A';
+	else if(per>=50&&per<70)
+		grade='B';
+    else if(per>=40&&per<50)
+		grade='C';
+	else
+		grade='F';
 }
 
-void ResizeDatabase()
+void student::getdata()
 {
-	database.resize(databaseSize);
-}
-
-void ShowStats()
-{
-	cout << "\nSIZE: " << databaseSize << endl;
-	cout << "STUDENT LIST (ROLL): ";
-
-	if (databaseSize == 0)
-	{
-		cout << "DATABASE IS EMPTY\n" << endl;
-		return;
-	}
-    else{
-        
-	for (int i = 0; i < databaseSize; i++)
-		cout << database[i].GetRoll()<<endl;
-
-	cout<< "\n" << endl;
-}
-
-}
-
-void Add()
-{
-	string name;
-	int roll;
-	float OOPs, ADE, DS, COSM, COA;
-
-	cout << "\nPLEASE ENTER THE DETAILS OF THE STUDENT:" << endl;
-	cout << "NAME: ";
+	cout<<"\nEnter The roll number of student ";
+	cin>>rollno;
+	cout<<"\n\nEnter The Name of student ";
 	cin.ignore();
-	getline(cin, name);
-	cout << "ROLL: ";
-	cin >> roll;
-	cout << "MARKS IN OOPs, ADE, DS, COSM AND COA: ";
-	cin >> OOPs >> ADE >> DS >> COSM >> COA;
-	cout << "\nSTUDENT DATA ADDED TO DATABASE" << endl;
-
-	databaseSize++;
-	ResizeDatabase();
-	database[databaseSize - 1].InsertData(name, roll, OOPs, ADE, DS, COSM, COA);
+	cin.getline(name,50);
+    cout<<"\nPlease enter marks out of 100 (Internals + External) "<<endl;
+	cout<<"\nEnter your marks in Data Structures in C : ";
+	cin>>DS_marks;
+	cout<<"\nEnter The marks in Object Oriented Programming using C++ : ";
+	cin>>OOPs_marks;
+	cout<<"\nEnter The marks in COSM : ";
+	cin>>COSM_marks;
+	cout<<"\nEnter The marks in Analog and Digital Electronics : ";
+	cin>>ADE_marks;
+	cout<<"\nEnter The marks in Computer Organization and Architecture: ";
+	cin>>COA_marks;
+	calculate();
 }
 
-void Delete(int roll)
+void student::showdata() const
 {
-	for (int i = 0; i < databaseSize; i++)
-		if (database[i].GetRoll() == roll)
-		{
-			database.erase(database.begin() + i);
-			cout << "\nSTUDENT WITH ROLL NUMBER" << roll << " HAS BEEN REMOVED FROM THE DATABASE\n" << endl;
-			databaseSize--;
-			return;
-		}
-
-	cout << "\nTHE ENTERED ROLL NUMBER: (" << roll << ") DOES NOT EXIST.\n" << endl;
+	cout<<"\nRoll number of student : "<<rollno;
+	cout<<"\nName of student : "<<name;
+	cout<<"\nMarks in Data Structures : "<<DS_marks;
+	cout<<"\nMarks in OOPS : "<<OOPs_marks;
+	cout<<"\nMarks in COSM : "<<COSM_marks;
+	cout<<"\nMarks in ADE : "<<ADE_marks;
+	cout<<"\nMarks in COA :"<<COA_marks;
+	cout<<"\nPercentage of student is  :"<<per;
+	cout<<"\nGrade of student is :"<<grade;
 }
+
+void student::show_tabular() const
+{
+	cout<<rollno<<setw(6)<<" "<<name<<setw(10)<<DS_marks<<setw(4)<<OOPs_marks<<setw(4)<<COSM_marks<<setw(4)
+		<<ADE_marks<<setw(4)<<COA_marks<<setw(8)<<per<<setw(6)<<grade<<endl;
+}
+
+int  student::retrollno() const
+{
+	return rollno;
+}
+
+void write_student();	//write the record in binary file
+void display_all();	//read all records from binary file
+void display_sp(int);	//accept rollno and read record from binary file
+void modify_student(int);	//accept rollno and update record of binary file
+void delete_student(int);	//accept rollno and delete selected records from binary file
+void class_result();	//display all records in tabular format from binary file
+void result();		//display result menu
+void intro();		//display welcome screen
+void entry_menu();	//display entry menu on screen
 
 int main()
 {
-	int choice, roll;
-
-	cout << "STUDENT REPORT MANAGEMENT" << endl;
-	cout << "==================================" << endl;
-
+	char ch;
+	cout.setf(ios::fixed|ios::showpoint);
+	cout<<setprecision(2); // program outputs decimal number to two decimal places
+	intro();
 	do
 	{
-		cout << "\n1. ADD NEW STUDENT DATA\n2. DELETE STUDENT DATA\n3. PRINT REPORT\n4. DATABASE STATS\n0. EXIT" << endl;
-		cout << "YOUR CHOICE: ";
-		cin >> choice;
-
-		switch (choice)
+		system("cls");
+		cout<<"\n\n\n\tSTUDENT REPORT MANAGEMENT SYSTEM";
+		cout<<"\n\n\t01. VIEW STUDENT REPORT DETAILS";
+		cout<<"\n\n\t02. ADD/DELETE/MODIFY REPORT";
+		cout<<"\n\n\t03. EXIT";
+		cout<<"\n\n\tPlease Select Your Option (1-3) ";
+		cin>>ch;
+		switch(ch)
 		{
-		case 1:
-			Add();
-			break;
-
-		case 2:
-			cout << "ROLL: ";
-			cin >> roll;
-			Delete(roll);
-			break;
-
-		case 3:
-			cout << "ROLL: ";
-			cin >> roll;
-			PrintReport(roll);
-			break;
-
-		case 4:
-			ShowStats();
-			break;
-
-		case 0:
-			break;
-
-		default:
-			cout << "\nINVALID CHOICE\n" << endl;
-			break;
+			case '1': result();
+				break;
+			case '2': entry_menu();
+				break;
+			case '3':
+				break;
+			default :cout<<"\a";
 		}
-	} while (choice);
-
+    }while(ch!='3');
 	return 0;
+}
+
+//***************************************************************
+//    	function to write in file
+//****************************************************************
+
+void write_student()
+{
+	student st;
+	ofstream outFile;
+	outFile.open("student.dat",ios::binary|ios::app);
+	st.getdata();
+	outFile.write(reinterpret_cast<char *> (&st), sizeof(student));
+	outFile.close();
+    	cout<<"\n\nStudent record Has Been Created ";
+	cin.ignore();
+	cin.get();
+}
+
+//***************************************************************
+//    	function to read all records from file
+//****************************************************************
+
+void display_all()
+{
+	student st;
+	ifstream inFile;
+	inFile.open("student.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	cout<<"\n\n\n\t\tDISPLAY ALL RECORD !!!\n\n";
+	while(inFile.read(reinterpret_cast<char *> (&st), sizeof(student)))
+	{
+		st.showdata();
+		cout<<"\n\n====================================\n";
+	}
+	inFile.close();
+	cin.ignore();
+	cin.get();
+}
+
+//***************************************************************
+//    	function to read specific record from file
+//****************************************************************
+
+void display_sp(int n)
+{
+	student st;
+	ifstream inFile;
+	inFile.open("student.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	bool flag=false;
+	while(inFile.read(reinterpret_cast<char *> (&st), sizeof(student)))
+	{
+		if(st.retrollno()==n)
+		{
+	  		 st.showdata();
+			 flag=true;
+		}
+	}
+	inFile.close();
+	if(flag==false)
+		cout<<"\n\nrecord not exist";
+	cin.ignore();
+	cin.get();
+}
+
+//***************************************************************
+//    	function to modify record of file
+//****************************************************************
+
+void modify_student(int n)
+{
+	bool found=false;
+	student st;
+	fstream File;
+	File.open("student.dat",ios::binary|ios::in|ios::out);
+	if(!File)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+    	while(!File.eof() && found==false)
+	{
+
+		File.read(reinterpret_cast<char *> (&st), sizeof(student));
+		if(st.retrollno()==n)
+		{
+			st.showdata();
+			cout<<"\n\nPlease Enter The New Details of student"<<endl;
+			st.getdata();
+		    	int pos=(-1)*static_cast<int>(sizeof(st));
+		    	File.seekp(pos,ios::cur);
+		    	File.write(reinterpret_cast<char *> (&st), sizeof(student));
+		    	cout<<"\n\n\t Record Updated";
+		    	found=true;
+		}
+	}
+	File.close();
+	if(found==false)
+		cout<<"\n\n Record Not Found ";
+	cin.ignore();
+	cin.get();
+}
+
+//***************************************************************
+//    	function to delete record of file
+//****************************************************************
+
+void delete_student(int n)
+{
+	student st;
+	ifstream inFile;
+	inFile.open("student.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	ofstream outFile;
+	outFile.open("Temp.dat",ios::out);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&st), sizeof(student)))
+	{
+		if(st.retrollno()!=n)
+		{
+			outFile.write(reinterpret_cast<char *> (&st), sizeof(student));
+		}
+	}
+	outFile.close();
+	inFile.close();
+	remove("student.dat");
+	rename("Temp.dat","student.dat");
+	cout<<"\n\n\tRecord Deleted ..";
+	cin.ignore();
+	cin.get();
+}
+
+//***************************************************************
+//    	function to display all students grade report
+//****************************************************************
+
+void class_result()
+{
+	student st;
+	ifstream inFile;
+	inFile.open("student.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		cin.ignore();
+		cin.get();
+		return;
+	}
+	cout<<"\n\n\t\tALL STUDENTS RESULT \n\n";
+	cout<<"==========================================================\n";
+	cout<<"R.No       Name       DS OOPS COSM ADE COA  %age   Grade"<<endl;
+	cout<<"==========================================================\n";
+	while(inFile.read(reinterpret_cast<char *> (&st), sizeof(student)))
+	{
+		st.show_tabular();
+	}
+	cin.ignore();
+	cin.get();
+	inFile.close();
+}
+
+//***************************************************************
+//    	function to display result menu
+//****************************************************************
+
+void result()
+{
+	char ch;
+	int rno;
+	system("cls");
+	cout<<"\n\n\n\tRESULT MENU";
+	cout<<"\n\n\n\t1. Class Result";
+	cout<<"\n\n\t2. Student Report Card";
+	cout<<"\n\n\t3. Back to Main Menu";
+	cout<<"\n\n\n\tEnter Choice (1/2/3)? ";
+	cin>>ch;
+	system("cls");
+	switch(ch)
+	{
+	case '1' :	class_result(); break;
+	case '2' :	cout<<"\n\n\tEnter Roll Number Of Student : "; cin>>rno;
+				display_sp(rno); break;
+	case '3' :	break;
+	default:	cout<<"\a";
+	}
+}
+
+//***************************************************************
+//    	INTRODUCTION FUNCTION
+//****************************************************************
+
+void intro()
+{
+    cout<<" \t______________________________________"<<endl;
+	cout<<"\t|             Welcome to               |"<<endl;
+	cout<<"\t|                                      |"<<endl;
+    cout<<"\t|                                      |"<<endl;
+	cout<<"\t| CSE B STUDENT REPORT CARD MANAGEMENT |"<<endl;
+	cout<<"\t|                                      |"<<endl;
+    cout<<"\t|______________________________________|"<<endl;
+	cout<<"\n\n\n\tPlease Enter Any Key To Start!";
+
+   cout<<"\n\n\n\n\n\n\n\n\n\tDeveloped by TEAM 1!"<<endl;
+
+	cin.get();
+}
+
+//***************************************************************
+//    	ENTRY / EDIT MENU FUNCTION
+//****************************************************************
+
+void entry_menu()
+{
+	char ch;
+	int num;
+	system("cls");
+	cout<<"\n\n\n\tENTRY MENU";
+	cout<<"\n\n\t1.CREATE STUDENT RECORD";
+	cout<<"\n\n\t2.DISPLAY ALL STUDENTS RECORDS";
+	cout<<"\n\n\t3.SEARCH STUDENT RECORD ";
+	cout<<"\n\n\t4.MODIFY STUDENT RECORD";
+	cout<<"\n\n\t5.DELETE STUDENT RECORD";
+	cout<<"\n\n\t6.BACK TO MAIN MENU";
+	cout<<"\n\n\tPlease Enter Your Choice (1-6) ";
+	cin>>ch;
+	system("cls");
+	switch(ch)
+	{
+	case '1':	write_student(); break;
+	case '2':	display_all(); break;
+	case '3':	cout<<"\n\n\tPlease Enter The roll number "; cin>>num;
+			display_sp(num); break;
+	case '4':	cout<<"\n\n\tPlease Enter The roll number "; cin>>num;
+			modify_student(num);break;
+	case '5':	cout<<"\n\n\tPlease Enter The roll number "; cin>>num;
+			delete_student(num);break;
+	case '6':	break;
+	default:	cout<<"\a"; entry_menu();
+	}
 }
